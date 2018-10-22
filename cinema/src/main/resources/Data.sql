7b2f24747ab1758a4e71d9eb21c4d2b1a525540a
Bodistvan <bodnarik.i@gmail.com>
2018-10-22 13:56:47 +0200

    movie tabla crud muveletei

--- cinema/src/main/resources/Data.sql
+++ cinema/src/main/resources/Data.sql
@@ -1,2 +1,5 @@
 insert into User(id,name,login) values (1,'Adam','adam@gmail.com');
-insert into User(id,name,login) values (2,'Gabor','gabor@gmail.com')
+insert into User(id,name,login) values (2,'Gabor','gabor@gmail.com');
+
+insert into Movie(id,name)values(1,'Függetlenség napja');
+insert into Movie(id,name)values(2,'Macskafogo');
