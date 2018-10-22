e9685173922b08a56a808adb7937e8378f12f111
Bodistvan <bodnarik.i@gmail.com>
2018-10-22 12:56:22 +0200

    felhasznalok crud muveletei

---
+++ cinema/src/main/resources/Data.sql
@@ -0,0 +1,2 @@
+insert into User(id,name,login) values (1,'Adam','adam@gmail.com');
+insert into User(id,name,login) values (2,'Gabor','gabor@gmail.com')
---
+++ cinema/src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+create table User(
+	id Long not null AUTO_INCREMENT,
+	name varchar(200),
+	login varchar(400)
+);
